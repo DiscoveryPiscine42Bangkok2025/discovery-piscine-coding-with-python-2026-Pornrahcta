faedd1bc5e0842baec41fd6e56a3935cebc58a81
Pornrachata <65010691@kmitl.ac.th>
2026-02-09 22:25:07 +0700

    Add Cellule5.11 - Python file

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,8 @@
+import sys
+
+if len(sys.argv) > 1:
+    print(f"parameters: {len(sys.argv) - 1}")
+    for i in range(1,4):
+        print(f"{sys.argv[i]}: {len(sys.argv[i])}")
+else:
+    print("none")
