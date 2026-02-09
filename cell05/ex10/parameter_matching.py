acb3c498028f61712605562f7b740ae4b9ff9e87
Pornrachata <65010691@kmitl.ac.th>
2026-02-09 22:25:02 +0700

    Add Cellule5.10 - Python file

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,11 @@
+import sys
+
+if len(sys.argv) == 2:
+    word = sys.argv[1]
+    user_input = input("What was the parameter? ")
+    if user_input == word:
+        print("Good job!")
+    else:
+        print("Nope, sorry...")
+else:
+    print("none")
