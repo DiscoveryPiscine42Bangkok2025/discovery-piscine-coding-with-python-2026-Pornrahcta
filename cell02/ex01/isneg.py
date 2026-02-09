7333edb78f804fc873088d285ad3d12c5085bba3
Pornrachata <65010691@kmitl.ac.th>
2026-02-09 16:30:45 +0700

    Add Cellule2.1 - Python file

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,11 @@
+user_input =  input()
+try:
+    number = int(user_input)
+    if number < 0:
+        print("This number is negative.")
+    elif number > 0:
+        print("This number is positive.")
+    else:
+        print("This number is both positive and negative.")
+except:
+    print("Input is not a number.")
