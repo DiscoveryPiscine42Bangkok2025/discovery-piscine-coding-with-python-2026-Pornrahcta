8d8d783ecf250108b425e8b869d6a54f84051808
Pornrachata <65010691@kmitl.ac.th>
2026-02-10 22:23:53 +0700

    Edit Cellule5.3 - Python file

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,4 +1,4 @@
 old_array = [2, 8, 9, 48, 8, 22,-12, 2]
-new_array = [x+2 for x in old_array]
+new_array = [x+2 for x in old_array if x > 5]
 print("Original array:", old_array)
-print("New array:", list(set(new_array[1:6])))
+print("New array:", list(set(new_array)))
